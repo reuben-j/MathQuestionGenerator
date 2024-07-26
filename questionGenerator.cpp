c9cd14c10ee93953467c9e3866f01a36b80f4252
reuben-j <reubenjarvela@gmail.com>
2024-07-26 16:03:40 +0100

    MoreCommits

--- questionGenerator.cpp
+++ questionGenerator.cpp
@@ -3,11 +3,24 @@
 #include <time.h>
 #include <tuple>
 
+
 using namespace std;
 
-int returnRandomNumInRange(int upper) {
+float returnRandomNumInRange(int upper) {
+    int sum = 1;
+    for(int i = 0; i < upper; i++) {
+        sum *= 10;
+    }
     srand(time(0));
-    return rand() % upper;
+    return rand() % sum;
+}
+float returnSmallerNumInRange(int upper) {
+    int sum = 1;
+    for(int i = 0; i < upper -2; i++) {
+        sum *= 10;
+    }
+    srand(time(0)+1);
+    return rand() % sum;
 }
 
 tuple<char, int> getInfo() {
@@ -24,19 +37,43 @@ tuple<char, int> getInfo() {
 
 int main() {
     
+    float actualAnswer = 0.00, userAnswer = 0.00;
     tuple<char, int> newTuple = getInfo();
     char div = '/', mult = '*', add = '+', sub = '-';
     
     if(get<0>(newTuple) == div) {
-        cout << "We shall divide with " << get<1>(newTuple) << " digits!";
+        cout << "We shall divide with " << get<1>(newTuple) << " digits!" << endl;
+        cout << "What is " << returnRandomNumInRange(get<1>(newTuple)) << " " << div << " " <<  returnSmallerNumInRange(get<1>(newTuple)) << "?";
+        actualAnswer = returnRandomNumInRange(get<1>(newTuple)) / returnSmallerNumInRange(get<1>(newTuple)) * 1.00;
+
+
     } else if(get<0>(newTuple) == mult) {
-        cout << "We shall multiply with " << get<1>(newTuple) << " digits!";
+        cout << "We shall multiply with " << get<1>(newTuple) << " digits!" << endl;
+        cout << "What is " << returnRandomNumInRange(get<1>(newTuple)) << " " << mult << " " << returnSmallerNumInRange(get<1>(newTuple)) << "?";
+        actualAnswer = returnRandomNumInRange(get<1>(newTuple)) * returnSmallerNumInRange(get<1>(newTuple)) * 1.00;
+
+
     } else if(get<0>(newTuple) == add) {
-        cout << "We shall add with " << get<1>(newTuple) << " digits!";
+        cout << "We shall add with " << get<1>(newTuple) << " digits!" << endl;
+        cout << "What is " << returnRandomNumInRange(get<1>(newTuple)) << " " << add << " " << returnSmallerNumInRange(get<1>(newTuple)) << "?";
+        actualAnswer = returnRandomNumInRange(get<1>(newTuple)) + returnSmallerNumInRange(get<1>(newTuple)) * 1.00;
+
+
     } else if(get<0>(newTuple) == sub) {
-        cout << "we shall subtract with " << get<1>(newTuple) << " digits!";
+        cout << "we shall subtract with " << get<1>(newTuple) << " digits!" << endl;
+        cout << "What is " << returnRandomNumInRange(get<1>(newTuple)) << " " << sub << " " << returnSmallerNumInRange(get<1>(newTuple)) << "?";
+        actualAnswer = returnRandomNumInRange(get<1>(newTuple)) - returnSmallerNumInRange(get<1>(newTuple)) * 1.00;
+
     } else {
         cout << "Improper choice";
     }
-     
+    
+    cout << endl << "Answer: ";
+    cin >> userAnswer;
+    if(userAnswer == roundf(actualAnswer * 100) / 100) {
+        cout << "Nice one, the answer was: " << roundf(actualAnswer * 100) / 100;
+    } else {
+        cout << "Wrong, the answer was: " << roundf(actualAnswer * 100) / 100;
+    }
+    
 }
